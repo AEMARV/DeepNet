bf78b5590236993e0b56fe83f577456df3cbda14
amir <a_emad@knights.ucf.edu>
2017-02-15 18:37:18 -0500

    Initial Framework

---
+++ startup.m
@@ -0,0 +1,9 @@
+addpath( '/home/student/Documents/MATLAB/MATCONVNET/matconvnet/matlab');
+vl_setupnn();
+rmpath(genpath('/home/student/Documents/MATLAB/MATCONVNET/matconvnet/matlab/simplenn'));
+rmpath(genpath('/home/student/Documents/MATLAB/MATCONVNET/matconvnet/examples'));
+addpath(genpath('./auxfunc'));
+addpath(genpath('./simplenn'));
+addpath(genpath('./layers'));
+addpath(genpath('./HyperParamIO'));
+addpath(genpath('./generalfuncs'));
